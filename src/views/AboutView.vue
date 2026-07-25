0a1e92cd92976dbb4c9236e309a3a8b30488bb0f
Cameron Osborn <cosborn.creates@gmail.com>
2026-07-25 10:57:59 -0400

    updated about us page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,29 +4,37 @@
       <div class="section-heading">About Us</div>
       <div class="section-content">
         <p>Welcome to our site! We are so excited for you to be here, and we have so much to tell you about!</p>
-        <p>RAD Collective is officially launching this month. Before constructing a new group vision and starting RAD, many of our members led and volunteered their efforts for the now-defunct PauseAI NYC, a local group of PauseAI US.</p>
-        <p>So who are we? We are NYC residents and folks from the surrounding area who all share a concern about AI safety, and <u>are willing to do something about it</u>. Our leaders and members come from a variety of backgrounds including (but not limited to) software engineering, teaching, web development, political organizing, AI research, art, film, and more! There is no prerequisite to join!</p>
+        <p>RAD Collective officially launched in July 2026. Before constructing a new group vision and starting RAD, many of our members led and volunteered their efforts for the now-defunct PauseAI NYC, a local group of PauseAI US.</p>
+        <p>So who are we? We are NYC residents and folks from the surrounding area who all share a concern about AI safety, and <u>are willing to do something about it</u>. Our leaders and members come from a variety of backgrounds including (but not limited to) software engineering, web development, political organizing, AI research, art, film, and more! There is no prerequisite to join!</p>
         <p>Our group is growing quickly, and we would love to have you join us if you're in the area.</p>
       </div>
     </div>
     <div class="section" id="what-we-do">
       <div class="section-heading">What We Do</div>
       <div class="section-content">
-        <p>We discuss and advocate for AI safety at multiple levels. We have group discussions at our general meetings, call our representatives, and canvas/table on the streets of NYC! We advocate for a global treaty on AI development, along with domestic state and federal-level legislation that promotes safety in the AI industry. We recently got a win at the NY state level with the passage of the <a href="https://www.nysenate.gov/legislation/bills/2025/A6453/amendment/A" target="_blank">RAISE Act</a> (sponsored by <a href="https://www.nytimes.com/2025/10/20/nyregion/alex-bores-ny-congress-primary.html">former congressional candidate Alex Bores</a>). This bill, while weaker than we would have hoped, serves as a huge milestone for AI regulation in the USA, being only the second bill of its kind nationwide (the other one being the <a href="https://legiscan.com/CA/text/SB53/id/3041703" target="_blank">California bill SB 53</a>). Our NYC group made hundreds of phone calls to the governor's office to move the needle and make that possible. If you want to make <i>real</i> change on this issue, we are the group to join.</p>
-        <p>We meet in real life!! Here is a photo of us before forming RAD, at the PauseAI NYC general meeting of February this year (please ignore the Halloween decorations, they give us a spooky vibe and we didn't just totally forget to take them down):</p>
-        <img src="@/assets/pause_general.jpg" alt="PauseAI February 2026 general meeting" style="width: 100%; border-radius: 8px;"/>
-        <p>But we also do other things, like canvassing and interviewing people in parks across the city! Here is a pic from an interview shoot we did in the winter:</p>
+        <p>We discuss and advocate for AI safety at multiple levels. We have group discussions at our general meetings, call our representatives, and canvas/table on the streets of NYC! We advocate for a global treaty on AI development, along with domestic state and federal-level legislation that promotes safety in the AI industry.</p>
+        <p>
+          We got a win at the NY state level last year with the passage of the <a href="https://www.nysenate.gov/legislation/bills/2025/A6453/amendment/A" target="_blank">RAISE Act</a>, sponsored by <a href="https://www.nytimes.com/2025/10/20/nyregion/alex-bores-ny-congress-primary.html">former congressional candidate Alex Bores</a>. This bill, while weaker than we would have hoped, serves as a huge milestone for AI regulation in the USA, being only the second bill of its kind nationwide (the other one being the <a href="https://legiscan.com/CA/text/SB53/id/3041703" target="_blank">California bill SB 53</a>). Our NYC group made hundreds of phone calls to the governor's office to move the needle and make that possible. If you want to make <i>real</i> change on this issue, we are the group to join.
+        </p>
+        <p>And in even more recent news, NY Governor Hochul has <a href="https://www.governor.ny.gov/executive-order/no-62-establishing-temporary-moratorium-data-centers-new-york-while-state-develops" target="_blank">signed an executive order</a> to halt the construction of "hyperscale" (over 50 megawatts of energy consumption per day) data centers for one year. This will give the NY government some time to study the impacts of existing data centers and develop a better regulatory framework. While we were not directly involved in driving this executive order, some of our members have attended and spoken out at town council meetings to help oppose individual data center projects. The data center issue has really been heating up, and it's more important than ever for people to organize against the build-up of this harmful infrastructure.</p>
+        <p>OK... now for the fun stuff.</p>
+        <p>We meet in real life!! It's always a good time. Here is a photo from our soft launch picnic on July 11, 2026:</p>
+        <img src="@/assets/rad_launch.jpg" alt="RAD Collective soft launch picnic" style="width: 100%; border-radius: 8px;"/>
+        <p>Also, as mentioned at the top of the page, many of us have been active even before forming RAD. Here is a photo from the PauseAI NYC general meeting of April this year:</p>
+        <img src="@/assets/pause_general_apr.jpg" alt="PauseAI April 2026 general meeting at Collider" style="width: 100%; border-radius: 8px;"/>
+        <p>And we do other things too, like canvassing and interviewing people in parks across the city! Here is a pic from an interview shoot we did all the way back in winter:</p>
         <img src="@/assets/interview_feb.jpg" alt="street interview" style="width: 100%; border-radius: 8px;"/>
-        <p>If you're interested in getting acquainted with our group, you should absolutely pull up to one of our <a href="https://luma.com/calendar/cal-uAkC2RDfO1aWULR" target="_blank">events</a>!</p>
+        <p>If you're interested in getting acquainted with our group, you should absolutely pull up to one of our <a href="https://luma.com/calendar/manage/cal-sw8QA8WDE2Uz9J4" target="_blank">events</a>!</p>
       </div>
     </div>
     <div class="section" id="fun-photos">
       <div class="section-heading">Additional fun photos!</div>
       <div class="section-content">
-        <p>Here are some amazin' photos of us eating pizza. We just do this sometimes for fun or if we're hungry. Not an official club event, but we live in New York and pizza is our #1 food group:</p>
+        <p>Here are some amazin' photos of us eating pizza. Not an official club event, but we live in New York and pizza is our #1 food group:</p>
         <img src="@/assets/pizza2.jpg" alt="members of RAD Collective eating pizza" style="width: 100%; border-radius: 8px;"/>
         <img src="@/assets/pizza1.jpg" alt="members of RAD Collective eating pizza again" style="width: 100%; border-radius: 8px;"/>
-        <p>Finally, I will leave you with the most beautiful creature that I know, the cat named Sunny <3<3<3</p>
+        <p><strong>Editorial note:</strong> Cameron thinks/knows that New Jersey pizza is better than New York pizza. This is not an official opinion of the group, but he will not be silenced. Please reach out if you want NJ pizza recs.</p>
+        <p>Finally, we will leave you with this beautiful creature, Sunny the cat <3<3<3</p>
         <img src="@/assets/sunny.jpg" alt="Ian's beautiful creature" style="width: 100%; border-radius: 8px;"/>
         <!-- TODO: Write the About Us content for RAD Collective.
              who we are, our mission, how people can get involved,
